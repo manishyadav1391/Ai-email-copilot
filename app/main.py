61cfbdcde62d6fda0de6fe0ac6c1f6afc7a3c56f
Manish <manishyadav940833@gmail.com>
2026-04-30 14:24:16 +0530

    cors added

--- app/main.py
+++ app/main.py
@@ -2,6 +2,27 @@ from app.services.email_service import connect_imap, fetch_unread_emails, parse_
 from fastapi import FastAPI
 from app.routes.email_routes import router as email_router
 from app.scheduler import start_scheduler
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+origins = [
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
 
 app = FastAPI(title="AI Email Copilot")
 
